7bcaa3539bc056e0f8eb3cf93927fa744d28b5ce
Gregor Pifko <gregor.pifko@gmail.com>
2025-05-27 22:21:36 +0200

    feat: add in-memory template caching to avoid redundant parsing on each request

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,8 +1,48 @@
 package main
 
-import "snippetbox.gregor-pifko/internal/models"
+import (
+	"html/template"
+	"path/filepath"
+
+	"snippetbox.gregor-pifko/internal/models"
+)
 
 type templateData struct {
 	Snippet  *models.Snippet
 	Snippets []*models.Snippet
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./ui/html/pages/*.tmpl")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+		// Parse base template
+		ts, err := template.ParseFiles("./ui/html/base.tmpl")
+		if err != nil {
+			return nil, err
+		}
+
+		// Add partials
+		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl")
+		if err != nil {
+			return nil, err
+		}
+
+		// Add page
+		ts, err = ts.ParseFiles(page)
+		if err != nil {
+			return nil, err
+		}
+
+		cache[name] = ts
+	}
+
+	return cache, nil
 }
