f091bd57773c1e2da46a4a40d44744234db3b636
Gregor Pifko <gregor.pifko@gmail.com>
2025-06-02 22:39:20 +0200

    feat: middleware for panic recovery

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
@@ -20,6 +21,21 @@ func (app *application) logRequest(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		app.infoLog.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
 
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Handles panic
+		defer func() {
+			if err := recover(); err != nil {
+				w.Header().Set("Connection", "close")
+				app.serverError(w, fmt.Errorf("panic recovered: %v", err))
+			}
+		}()
+
+		// Code that may panic
 		next.ServeHTTP(w, r)
 	})
 }
